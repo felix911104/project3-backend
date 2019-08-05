5db823f8c6cb9983424582e3e26955f13c761aca
Adam Minkus <adam.minkus@gmail..com>
2019-08-05 11:26:38 -0700

    added mock index.js files

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 const axios = require("axios");
 const bcrypt = require('bcrypt');
 const routes = require("./routes");
+const cors = require("cors");
 // const saltRounds = 10;
 
 const app = express();
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 // Static directory
 app.use(express.static("public"));

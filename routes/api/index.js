d8e8116e4d9dbb3404a2b2ff1fdb13070a593a04
Adam Minkus <adam.minkus@gmail..com>
2019-08-06 14:50:00 -0700

    small change

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,19 +3,23 @@ var db = require("../../models")
 const bcrypt = require('bcrypt');
 
 module.exports = function (app) {
-app.get("/", function(req, res) {
-  db.User.findAll({}).then(result => {
-    res.send(result)
-  })
-})
 
-  //login
+//route to get all users in db
+// app.get("/", function(req, res) {
+//   db.User.findAll({}).then(result => {
+//     res.send(result)
+//   })
+// })
+
+  //login for existing user
   app.get("/api/users/:name/:password", function (req, res) {
 
     console.log(req.params.password);
     console.log(req.params.name);
+    //find user in db
     db.User.findOne({ where: { name: req.params.name } }).then(dbUser => {
       console.log(dbUser);
+      //comparing hashed password
       let loggedIn = bcrypt.compareSync(req.params.password, dbUser.password);
       if (loggedIn) {
 
@@ -30,9 +34,10 @@ app.get("/", function(req, res) {
     });
   });
 
-  //sign up
+  //sign up for new user
   app.post('/api/users', function (req, res) {
     console.log(req.body)
+    //find or create new user
     db.User.findOrCreate({ where: {name: req.body.name}, defaults: {password: req.body.password} }).then(([user, created]) => {
        console.log(user.get({
        plain: true

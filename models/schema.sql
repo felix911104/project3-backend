7018d4f1014e5fcbd19614f7e94bde256a078354
Adam Minkus <adam.minkus@gmail..com>
2019-08-09 11:49:22 -0700

    added shelter api

--- models/schema.sql
+++
@@ -1,7 +0,0 @@
-
--- USE sheltr_db;
--- INSERT INTO users (name, password)
--- VALUES ("weimin", "1234567");
--- INSERT INTO food (day_time,meal_served,people_served,location,name_of_program)
--- VALUES ("example","example","example","example","example");
-
